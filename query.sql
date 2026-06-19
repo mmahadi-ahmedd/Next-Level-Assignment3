31618cd7b33ded88add228d8c1296e27ec8ffa9e
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2026-06-20 01:47:08 +0600

    added query 1 and 2

--- query.sql
+++ query.sql
@@ -18,3 +18,24 @@ CREATE TABLE matches (
     match_status VARCHAR(20) NOT NULL
         CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
+
+CREATE TABLE bookings (
+    booking_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(10),
+    payment_status VARCHAR(20)
+        CHECK (payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost DECIMAL(10,2) NOT NULL
+        CHECK (total_cost >= 0),
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_match
+        FOREIGN KEY (match_id)
+        REFERENCES matches(match_id)
+        ON DELETE CASCADE
+)
