87142966dea5f76f6541b36bbedf1c7c029589b5
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2026-06-20 01:57:01 +0600

    added query 7

--- query.sql
+++ query.sql
@@ -105,3 +105,10 @@ SELECT
 FROM users u
 LEFT JOIN bookings b ON u.user_id = b.user_id
 ORDER BY u.user_id;
+
+
+-- Query 6: Subquery (above average cost)
+
+SELECT booking_id, match_id, total_cost
+FROM bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM bookings);
