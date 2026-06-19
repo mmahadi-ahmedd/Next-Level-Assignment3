8bb7e71d89b181647cd6bd26c53098c904486751
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2026-06-20 01:52:27 +0600

    added query 5

--- query.sql
+++ query.sql
@@ -83,3 +83,15 @@ SELECT
     COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM bookings
 WHERE payment_status IS NULL;
+
+-- Query 4: INNER JOIN
+
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN matches m ON b.match_id = m.match_id
+ORDER BY b.booking_id;
