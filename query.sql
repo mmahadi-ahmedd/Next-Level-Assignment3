7f671bc676a9c0a115447eb3934a708faac1fef2
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2026-06-20 01:39:29 +0600

    created matches table

--- query.sql
+++ query.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE football_ticketing;
 
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    role VARCHAR(20) NOT NULL
+        CHECK (role IN ('Ticket Manager', 'Football Fan')),
+    phone_number VARCHAR(20)
+);
